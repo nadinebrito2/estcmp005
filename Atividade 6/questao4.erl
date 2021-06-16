0596c9de91d6b62fb1589b120e740c6d87b448dd
Nadine da Cunha Brito <61133008+nadinebrito2@users.noreply.github.com>
2021-06-16 17:35:15 -0400

    Update questao4.erl

--- Atividade 6/questao4.erl
+++ Atividade 6/questao4.erl
@@ -1,11 +1,7 @@
 -module(questao4).
--import(lists,[append/2]). 
--export([start/0]). 
+-export([start/0]).
 
-start() -> 
-   Lst1 = [1,2,3], 
-   Lst2 = [3,5,7,9],
-   Lst3 = append(Lst2,Lst1),
-   Set = sets:from_list(Lst3),
-   Set2 = sets:to_list(Set),
-   lists:sort(Set2).
+start() ->
+   Lista1 = [1,9,3],
+   Lista2 = [4,5,6],
+   Lista3 = [Lista1 ++ Lista2].
