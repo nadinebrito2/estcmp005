12784b0e77002b67b5d1789f980228c2614356bf
Nadine da Cunha Brito <61133008+nadinebrito2@users.noreply.github.com>
2021-06-15 22:03:55 -0400

    Update questao3.erl

--- Atividade 6/questao3.erl
+++ Atividade 6/questao3.erl
@@ -1,11 +1,10 @@
 -module(questao3).
--import(lists,[append/2]). 
--import(lists,[merge/2]). 
--export([start/0]). 
 
-start() -> 
-   Lst1 = [1,2,3], 
-   Lst2 = [3,4,5],
-   Lst3 = merge(Lst1,Lst2),
+-import(lists,[append/2]).
+
+-export([start/2]). 
+
+start(Lst1,Lst2) ->
+   Lst3 = append(Lst1,Lst2),
    Set = sets:from_list(Lst3),
    sets:to_list(Set).
