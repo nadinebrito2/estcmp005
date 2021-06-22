8f86809206fe45ff365d1063ecc1fc03357affbf
Nadine da Cunha Brito <61133008+nadinebrito2@users.noreply.github.com>
2021-06-22 19:11:46 -0400

    Update fibonacci.hs

--- Atividade 7/fibonacci.hs
+++ Atividade 7/fibonacci.hs
@@ -1,7 +1,7 @@
 module Main where
 
-fibonacci_aux num parcial = if num == 1 then parcial else fibonacci_aux (num-1) (parcial*num)
-fibonacci_cauda num = fibonacci_aux num 1
+fibonacci_aux num a b = if num == 0 then a else if num == 1 then b else fibonacci_aux (num-1) b (a + b)
+fibonacci_cauda num = fibonacci_aux num 0 1
 
 main = do n <- readLn
           print(fibonacci_cauda n)
