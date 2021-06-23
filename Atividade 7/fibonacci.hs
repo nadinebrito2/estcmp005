11f9b71359108e3a25abf8f87dc6f787cfe7bd09
Nadine da Cunha Brito <61133008+nadinebrito2@users.noreply.github.com>
2021-06-23 11:19:22 -0400

    Update fibonacci.hs

--- Atividade 7/fibonacci.hs
+++ Atividade 7/fibonacci.hs
@@ -1,4 +1,4 @@
-module Main where
+module Fibonacci (fibonacci_cauda) where
 
 fibonacci_aux num a b = if num == 0 then a else if num == 1 then b else fibonacci_aux (num-1) b (a + b)
 fibonacci_cauda num = fibonacci_aux num 0 1
