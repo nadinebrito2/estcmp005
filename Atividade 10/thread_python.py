dd7192cf08acaef6f80333c802ef4ac01ef53f82
Nadine da Cunha Brito <61133008+nadinebrito2@users.noreply.github.com>
2021-06-29 21:30:25 -0400

    Update thread_python.py

--- Atividade 10/thread_python.py
+++ Atividade 10/thread_python.py
@@ -3,19 +3,19 @@ import time
 
 inicio = time.time()
 def worker(message):
-    for i in range(10):
-        print (message,i)
+    for i in range(1000):
+        print('\n',message,i,'')
         time.sleep(1)
 
-
-t = Thread(target=worker,args=("thread sendo executada",))
-t.start()
+for i in range (1000):
+    t = Thread(target=worker,args=("\nthread sendo executada",))
+    t.start()
+    
 while t.is_alive():
-    print ("Aguardando thread")
-    time.sleep(10)
+    print ("\nAguardando thread")
+    time.sleep(1)
 
-t = time.time()
 print ("Thread morreu")
 print ("Finalizando programa")
 fim = time.time()
-print("tempo de exec:",fim - inicio)
+print("tempo de execução:",fim - inicio)
